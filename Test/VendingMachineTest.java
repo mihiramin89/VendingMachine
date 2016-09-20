3628964d521a2c801dc628fe2bc0d72a0c8cbf9f
Mihir Amin <mihiramin89@gmail.com>
2016-09-20 13:08:04 -0400

    added test cases for selecting a product

--- Test/VendingMachineTest.java
+++ Test/VendingMachineTest.java
@@ -1,5 +1,13 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Code.ProductEnum;
+import Code.VendingMachine;
+
 public class VendingMachineTest {
 
 	private VendingMachine mach;
@@ -44,4 +52,95 @@ public class VendingMachineTest {
 	{
 		assertEquals("0.0", mach.EnterCoin(1.0).toString());
 	}
+
+	/************************************************
+	*
+	* Test Cases for VendingMachine > SelectProduct
+	* 
+	**************************************************/
+	@Test
+	public void SelectOneColaFromVendingMachine()
+	{
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.25);
+		assertEquals("THANK YOU", mach.SelectProduct(ProductEnum.COLA));
+	}
+	
+	@Test
+	public void SelectOneChipFromVendingMachine()
+	{
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.25);
+		assertEquals("THANK YOU", mach.SelectProduct(ProductEnum.CHIPS));
+	}
+	@Test
+	public void SelectOneCandyFromVendingMachine()
+	{
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.25);
+		mach.EnterCoin(0.10);
+		mach.EnterCoin(0.05);
+		assertEquals("THANK YOU", mach.SelectProduct(ProductEnum.CANDY));
+	}
+	
+	@Test
+	public void SelectColaWithoutEnteringMoneyFromVendingMachine()
+	{
+		assertEquals("Price: 1.00", mach.SelectProduct(ProductEnum.COLA));
+	}
+	
+	@Test
+	public void SelectChipWithoutEnteringMoneyFromVendingMachine()
+	{
+		assertEquals("Price: 0.50", mach.SelectProduct(ProductEnum.CHIPS));
+	}
+	
+	@Test
+	public void SelectCandyWithoutEnteringMoneyFromVendingMachine()
+	{
+		assertEquals("Price: 0.65", mach.SelectProduct(ProductEnum.CANDY));
+	}
+	
+	@Test
+	public void SelectColasWhenSoldOutFromVendingMachine()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.25);
+			mach.SelectProduct(ProductEnum.COLA);
+		}
+		assertEquals("SOLD OUT",mach.SelectProduct(ProductEnum.COLA));
+	}
+	
+	@Test
+	public void SelectChipsWhenSoldOutFromVendingMachine()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.25);
+			mach.SelectProduct(ProductEnum.CHIPS);
+		}
+		assertEquals("SOLD OUT",mach.SelectProduct(ProductEnum.CHIPS));
+	}
+	
+	@Test
+	public void SelectCandyWhenSoldOutFromVendingMachine()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.25);
+			mach.EnterCoin(0.10);
+			mach.EnterCoin(0.05);
+			mach.SelectProduct(ProductEnum.CANDY);
+		}
+		assertEquals("SOLD OUT",mach.SelectProduct(ProductEnum.CANDY));
+	}
+
 }
